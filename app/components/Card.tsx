c3096eae8ffbeb1db1f61e20c59a09a18a1a767e
Valeria Trofim <valeria.trofim17@gmail.com>
2025-01-18 03:16:24 +0100

    Update the Card content

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,29 +1,39 @@
-import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Card = () => {
+  const cardArray = [
+    { id: 1, title: "CSS", image: "css" },
+    { id: 2, title: "HTML", image: "html5" },
+    { id: 3, title: "JS", image: "js" },
+    { id: 4, title: "React", image: "react" },
+    { id: 5, title: "TS", image: "ts" },
+  ];
   return (
-    <div className="max-w-sm bg-transparent hover:bg-slate-800 rounded-lg  ">
-      <a href="#">
-        <Image
-          className="rounded-t-lg"
-          src="/cardimg/css.jpg"
-          alt="image"
-          width={400}
-          height={300}
-        />
-      </a>
-      <div className="p-5">
-        <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-indigo-100">
-            CSS Questions
-          </h5>
-        </a>
-        <p className="mb-3 font-normal text-indigo-200">
-          Test your CSS skills and unlock your web design potential—try our CSS
-          quiz now and see how you stack up!
-        </p>
-      </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {cardArray.map((item, id) => (
+        <div
+          key={id}
+          className="max-w-sm bg-transparent hover:bg-slate-800 rounded-lg  "
+        >
+          <Link href="#">
+            <Image
+              className="rounded-t-lg"
+              src={"/assets/" + item.image + ".png"}
+              alt="image"
+              width={400}
+              height={300}
+            />
+          </Link>
+          <div className="p-5">
+            <Link href="#">
+              <h5 className="mb-2 text-2xl text-center font-bold tracking-tight text-indigo-100">
+                {item.title + " " + "Quiz"}
+              </h5>
+            </Link>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
